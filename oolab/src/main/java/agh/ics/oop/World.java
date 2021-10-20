f28e9783e61fa8d7eeadca49eaf0b582c4d2a826
SzymczakJ <szymczak.jakub12@gmail.com>
2021-10-20 11:39:57 +0200

    lab1

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -13,7 +13,7 @@ public class World {
         Direction[] directions;
         directions = new Direction[args.length];
         for (int i = 0; i < args.length; i++) {
-            directions[i] = switch(args[i]) {
+            directions[i] = switch (args[i]) {
                 case "f" -> Direction.FORWARD;
                 case "b" -> Direction.BACKWARD;
                 case "r" -> Direction.RIGHT;
@@ -26,7 +26,7 @@ public class World {
 
     public static void run(Direction[] args) {
 //        System.out.println("zwierzak idzie do przodu");
-        for (Direction arg: args) {
+        for (Direction arg : args) {
 //            if (!Objects.equals(arg, args[args.length - 1])) System.out.print(arg + ",");
 //            else System.out.println(arg);
             switch (arg) {
